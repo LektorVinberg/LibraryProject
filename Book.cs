163e76c47834c46927f8fecbae3087e6aeaad496
larsg <larsg@GRANDAT>
2026-04-28 17:38:17 +0200

    Add project files.

---
+++ Book.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Libra
+{
+    internal class Book
+    {
+        string Title { get; set; }
+        string Author { get; set; }
+        String ISBN { get; set; }
+        BookState Status { get; set; }
+
+        internal Book(string title, string author, string isbn, BookState status)
+        {
+            Title = title;
+            Author = author;
+            ISBN = isbn;
+            Status = status;
+        }
+
+        internal string GetDetails()
+        {
+            return $"Title: {Title}, Author: {Author}, ISBN: {ISBN}, Status: {Status}";
+        }
+
+        internal void SetStatus(BookState status)
+        {
+            Status = status;
+        }
+
+    }
+
+    enum BookState
+    {
+        Available,
+        OnLoan
+    }
+}
